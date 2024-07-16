03c096b7ea46263395c23111e64830f3ef5afab2
tanmay-8 <tanmayshingde8@gmail.com>
2024-07-16 21:05:12 +0530

    some work done

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,14 @@
-import React from "react";
-import ButtonGradient from "./assets/svg/ButtonGradient";
-import Benefits from "./components/Benefits";
-import Collaboration from "./components/Collaboration";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Hero from "./components/Hero";
-import MainScreen from "./components/MainScreen";
-import Pricing from "./components/Pricing";
-import Roadmap from "./components/Roadmap";
-import Services from "./components/Services";
-import "./App.css";
-
+import React from 'react'
+import { Routes ,Route} from 'react-router-dom'
+import Home from './pages/Home'
 const App = () => {
   return (
-    <div className="gradient-background">
-      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-        <Header />
-        <MainScreen />
-        <Hero />
-        <Benefits />
-        <Collaboration />
-        <Services />
-        <Pricing />
-        <Roadmap />
-        <Footer />
-      </div>
-      <ButtonGradient />
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import ButtonGradient from "../assets/svg/ButtonGradient";
+import Benefits from "../components/Benefits";
+import Collaboration from "../components/Collaboration";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+import MainScreen from "../components/MainScreen";
+import Pricing from "../components/Pricing";
+import Roadmap from "../components/Roadmap";
+import Services from "../components/Services";
+
+const Home = () => {
+  return (
+    <div className="gradient-background">
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+        <MainScreen />
+        <Hero />
+        <Benefits />
+        <Collaboration />
+        <Services />
+        <Pricing />
+        <Roadmap />
+        <Footer />
+      </div>
+      <ButtonGradient />
+    </div>
+  );
+};
+
+export default Home;
