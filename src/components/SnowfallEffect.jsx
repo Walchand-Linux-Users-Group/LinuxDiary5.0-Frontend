825920d68a48ad87357d9392de785ad762580d56
tanmay-8 <tanmayshingde8@gmail.com>
2024-07-27 16:50:00 +0530

    snowfall

--- src/components/SnowfallEffect.jsx
+++ src/components/SnowfallEffect.jsx
@@ -4,7 +4,7 @@ import Snowfall from "react-snowfall";
 
 const SnowfallEffect = () => (
   <div className="fixed top-0 left-0 w-full h-full pointer-events-none z-25">
-    <Snowfall snowflakeCount={200} />
+    <Snowfall snowflakeCount={150} />
   </div>
 );
 
