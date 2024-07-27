5b7820c6ae57ace78de7db349db1bb1d9d85462b
thundersp <thundersp20504@gmail.com>
2024-07-27 16:22:33 +0530

    Changes in Distro Spinning

--- src/constants/index.js
+++ src/constants/index.js
@@ -116,7 +116,7 @@ export const roadmap = [
 ];
 
 export const collabText =
-  "With smart automation and top-notch security, it's the perfect solution for teams looking to work smarter.";
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
 export const collabContent = [
   {
