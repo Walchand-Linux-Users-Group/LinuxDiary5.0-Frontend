639c248f400c2c72630f4e37bff4873756fca8ec
tanmay-8 <tanmayshingde8@gmail.com>
2024-07-29 12:22:51 +0530

    centering diary

--- src/components/Flipbook.jsx
+++ src/components/Flipbook.jsx
@@ -13,9 +13,10 @@ import Img8 from "../assets/diary/Img8.jpg";
 import Img9 from "../assets/diary/Img9.jpg";
 import Img10 from "../assets/diary/Img10.jpg";
 import Img11 from "../assets/diary/Img11.jpg";
+import Img12 from "../assets/diary/Img12.jpg";
 
 const Flipbook = () => {
-    const [dimensions, setDimensions] = useState({ width: 400, height: 600 });
+    const [dimensions, setDimensions] = useState({ width: 400, height: 500 });
     const [isClosed, setIsClosed] = useState(true);
     const images = [
         Img1,
@@ -25,6 +26,7 @@ const Flipbook = () => {
         Img5,
         Img6,
         Img7,
+        Img12,
         Img8,
         Img9,
         Img10,
@@ -34,64 +36,52 @@ const Flipbook = () => {
     useEffect(() => {
         setIsClosed(true);
     }, []);
-    return (<>
-        <div className="text-white font-bold text-center text-3xl lg:text-5xl ">Previous year LinuxDiary</div>
-        <div className="w-screen flex items-center justify-center overflow-visible p-4 sm:p-16">
-            <HTMLFlipBook
-                width={dimensions.width}
-                height={dimensions.height}
-                size="stretch"
-                minWidth={300}
-                maxWidth={window.innerWidth < 400 ? 350 : 500}
-                minHeight={450}
-                maxHeight={600}
-                maxShadowOpacity={0.5}
-                showCover={true}
-                mobileScrollSupport={true}
-                onFlip={(e) => {
-                    if (e.data === 0) {
-                        setIsClosed(true);
-                    } else {
-                        setIsClosed(false);
-                    }
-                }}
-                startPage={0}
-                className={`translate-x-0 transition-all md:${
-                    isClosed ? "-translate-x-[25%]" : "-translate-x-0"
-                }`}
-                id="flipbook"
-            >
-                {/* Cover Page */}
-                <div className="">
-                    <img
-                        src={CoverImg}
-                        alt="cover"
-                        className="w-full h-full"
-                    ></img>
-                </div>
-                {/* Inner Pages */}
-                {[...Array(6)].map((_, index) => (
-                    <div
-                        key={index}
-                        className="bg-[#fafafa] rounded-lg h-full w-full shadow-custom-inset"
-                    >
-                        <div className="w-full h-full flex flex-col justify-center items-center space-y-6 sm:space-y-12 z-10 p-4 sm:p-8">
-                            <Suspense
-                                fallback={
-                                    <img
-                                        src={PlaceholderImg}
-                                        alt={`placeholder ${index * 2 + 1}`}
-                                        className="w-full h-auto"
-                                    ></img>
-                                }
-                            >
-                                <img
-                                    src={images[index * 2]}
-                                    alt={`placeholder ${index * 2 + 2}`}
-                                    className="w-full h-auto"
-                                />
-                            </Suspense>
-                            {[index * 2 + 1] < 11 && (
+    return (
+        <>
+            <div className="text-white font-bold text-center text-3xl lg:text-5xl ">
+                Previous year LinuxDiary
+            </div>
+            <div className="w-screen flex items-center justify-center overflow-hidden p-4 sm:p-16">
+                <HTMLFlipBook
+                    width={dimensions.width}
+                    height={dimensions.height}
+                    size="stretch"
+                    minWidth={300}
+                    maxWidth={window.innerWidth < 400 ? 350 : 500}
+                    minHeight={450}
+                    maxHeight={500}
+                    maxShadowOpacity={0.5}
+                    showCover={true}
+                    mobileScrollSupport={true}
+                    onFlip={(e) => {
+                        if (e.data === 0) {
+                            setIsClosed(true);
+                        } else {
+                            setIsClosed(false);
+                        }
+                    }}
+                    startPage={0}
+                    style={{
+                        marginLeft:(window.innerWidth > 768 && isClosed)?"-25%":"0" ,
+                    }}
+                    id="flipbook"
+                    className="transition-all"
+                >
+                    {/* Cover Page */}
+                    <div className="">
+                        <img
+                            src={CoverImg}
+                            alt="cover"
+                            className="w-full h-full"
+                        ></img>
+                    </div>
+                    {/* Inner Pages */}
+                    {[...Array(12)].map((_, index) => (
+                        <div
+                            key={index}
+                            className="bg-[#fafafa] rounded-lg h-full w-full shadow-custom-inset"
+                        >
+                            <div className="w-full h-full flex flex-col justify-center items-center space-y-6 sm:space-y-12 z-10 p-4 sm:p-8">
                                 <Suspense
                                     fallback={
                                         <img
@@ -102,17 +92,16 @@ const Flipbook = () => {
                                     }
                                 >
                                     <img
-                                        src={images[index * 2 + 1]}
+                                        src={images[index]}
                                         alt={`placeholder ${index * 2 + 2}`}
                                         className="w-full h-auto"
                                     />
                                 </Suspense>
-                            )}
+                            </div>
                         </div>
-                    </div>
-                ))}
-            </HTMLFlipBook>
-        </div>
+                    ))}
+                </HTMLFlipBook>
+            </div>
         </>
     );
 };
