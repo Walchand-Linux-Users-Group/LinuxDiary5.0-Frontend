075c051a7eb185918ee84a76c61df7e76779a93c
tanmay-8 <tanmayshingde8@gmail.com>
2024-07-30 15:06:37 +0530

    cleanup

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,12 @@
 // src/pages/Home.js
 import React from "react";
 import ButtonGradient from "../assets/svg/ButtonGradient";
-import Benefits from "../components/Benefits";
 import Collaboration from "../components/Collaboration";
 import Footer from "../components/Footer";
-import Hero from "../components/Hero";
 import MainScreen from "../components/MainScreen";
-import Pricing from "../components/Pricing";
-import Roadmap from "../components/Roadmap";
-import Services from "../components/Services";
 import Prizes from "../components/Prizes";
-import SnowfallEffect from "../components/SnowfallEffect"; // Import SnowfallEffect
-import Wargames from "../components/Wargames";
+import SnowfallEffect from "../components/SnowfallEffect";
 import Session from "../components/Session/Session";
-import CardCollection from "../components/CardCollection/CardCollection";
 import Register from "../components/Register";
 import Flipbook from "../components/Flipbook";
 
