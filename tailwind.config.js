bb9b831124c4e24c80c5cb3f1c7c831fe2688c2d
tanmay-8 <tanmayshingde8@gmail.com>
2024-07-31 18:32:05 +0530

    sponsors and change in form

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,22 @@ export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
         extend: {
+            screens: {
+                sm: "640px",
+                // => @media (min-width: 640px) { ... }
+
+                md: "920px",
+                // => @media (min-width: 768px) { ... }
+
+                lg: "1024px",
+                // => @media (min-width: 1024px) { ... }
+
+                xl: "1280px",
+                // => @media (min-width: 1280px) { ... }
+
+                "2xl": "1536px",
+                // => @media (min-width: 1536px) { ... }
+            },
             colors: {
                 cardBlue: "#0F4C82",
                 cardGrey: "#A0A6C3",
